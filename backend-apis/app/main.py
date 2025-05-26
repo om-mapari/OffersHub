8c966538375b5ec43130a30fd08d3cbde776ec47
Om Mapari <mapariom05@gmail.com>
2025-05-26 07:47:25 +0530

    🐳Docker File Added

--- backend-apis/app/main.py
+++ backend-apis/app/main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from middlewares.logging_middleware import log_requests
 from db.base import Base, UserBase, engine, user_engine
-from controllers import auth_controller, user_controller, admin_controller, tenant_controller
-
+# from controllers import auth_controller, user_controller, admin_controller, tenant_controller
+from controllers import user_controller
 app = FastAPI()
 
 # DB Table Creation
@@ -23,7 +23,7 @@ app.add_middleware(
 app.middleware("http")(log_requests)
 
 # Routers
-app.include_router(auth_controller.router)
+# app.include_router(auth_controller.router)
 app.include_router(user_controller.router)
-app.include_router(admin_controller.router)
-app.include_router(tenant_controller.router)
+# app.include_router(admin_controller.router)
+# app.include_router(tenant_controller.router)
