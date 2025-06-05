b7d955601622acb730f0924f8957bb7b58424006
V Gupta <bh01487186@devcorptenant.com>
2025-06-05 07:30:37 +0530

    Email Sender

---
+++ backend-apis/app/email_sender.py
@@ -0,0 +1,52 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import markdown
+import sys
+
+#handle null values in the script if null then put "" in the place of null value
+recipient_email, campaign_name = sys.argv[1:3] if len(sys.argv) > 2 else ("vishalgupta1504@gmail.com", "Campaign Name")
+
+def send_email(recipient_email, campaign_name):
+    def send_email_using_gmail(email, password, recipient_email, subject, markdown_body):
+        html_body = markdown.markdown(markdown_body)
+        
+        msg = MIMEMultipart()
+        msg['From'] = email
+        msg['To'] = recipient_email
+        msg['Subject'] = subject
+        
+        msg.attach(MIMEText(html_body, 'html'))
+
+        try:
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()  # Start TLS encryption
+            server.login(email, password)
+            server.sendmail(email, recipient_email, msg.as_string())
+            print("Email sent successfully!")
+            
+        except Exception as e:
+            print(f"Failed to send email. Error: {str(e)}")
+        
+        finally:
+            server.quit()
+
+    email = "offershubbarclays@gmail.com"
+    password = "pany jzht ucrv lfwv" 
+    subject = "Campaign Selection Notification"
+    markdown_body = f"""
+    # Congratulations!
+
+    We are excited to inform you that you have been selected for the following campaign:
+
+    ### **Campaign Name**
+    *{campaign_name}*
+
+    We look forward to your participation!
+    Thank you for being a valued member of our community.
+
+    ### **Best Regards,**
+    ### **Offers Hub Team**
+    """
+
+    send_email_using_gmail(email, password, recipient_email, subject, markdown_body)
